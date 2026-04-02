c5afaa2bee8730cd83492c23b0b9eb1ea82d790c
wenlang.xu <simbax0913@gmail.com>
2026-04-02 17:45:03 +0800

    [Player][feat]新增人物水平移动翻转

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Components")]
     public Rigidbody2D rb;
     public Animator am; 
+    public SpriteRenderer sr;
+
+    [Header("Public Variables")]
     public float moveSpeed = 1.0f;
     private Vector2 moveDirection;
 
@@ -30,5 +34,10 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = moveDirection * moveSpeed;
         am.SetBool("IsRunning", moveDirection.magnitude > 0.1f);
+        
+        if (moveDirection.magnitude > 0.1f && Mathf.Abs(moveDirection.x) > 0.1f) // Only flip the sprite if the player is moving horizontally
+        {
+            sr.flipX = moveDirection.x < 0; // flip the sprite based on horizontal movement direction
+        }
     }
 }
